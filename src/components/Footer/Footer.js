f7a6ce07bab12396d3d03290707de90c32b91f74
Mohmed <mohmedmahmoudfouad@gmail.com>
2022-07-26 22:01:13 +0200

    Quran

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Footer.css'
-import {Link} from 'react-router-dom'
 
 const Footer = () => {
   return (
@@ -10,11 +9,10 @@ const Footer = () => {
        </div>
        <div className='Footer-content'>
           <div className='socil'>
-             <Link to='https://www.facebook.com/Anamohmed135'><i class="fa fa-brands fa-facebook-square"></i></Link>
-             <Link to='https://www.linkedin.com/in/mohmedmahmoudfouad'><i class="fa fa-brands fa-linkedin"></i></Link>
-             <Link to='https://twitter.com/Anamohmed00?t=4Rd2VX0oJqd3EKIfPlCKsA&s=08'><i class="fa fa-brands fa-twitter"></i></Link>
-             <Link to='http://wa.me/+201157068530'><i class="fa fa-brands fa-whatsapp-square"></i></Link>
-             <Link to='https://www.facebook.c'><i class="fa fa-solid fa-envelope"></i></Link>
+             <a href='https://www.facebook.com/Anamohmed135'><i class="fa fa-brands fa-facebook-square"></i></a>
+             <a href='https://www.linkedin.com/in/mohmedmahmoudfouad'><i class="fa fa-brands fa-linkedin"></i></a>
+             <a href='https://twitter.com/Anamohmed00?t=4Rd2VX0oJqd3EKIfPlCKsA&s=08'><i class="fa fa-brands fa-twitter"></i></a>
+             <a href='http://wa.me/+201157068530'><i class="fa fa-brands fa-whatsapp-square"></i></a>
           </div>
        </div>
     </div>
